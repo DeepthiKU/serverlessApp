fdd1d3a3ebc4a4ef86a2e7dcd0b1beffb1d8310a
DeepthiKU <133627731+DeepthiKU@users.noreply.github.com>
2025-01-08 10:30:34 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_Qi8wuMZCd', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '63j1658v719f20a517np768hj2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-2' // e.g. us-east-2
+        userPoolId: 'ap-south-1_EjOR9yVH1', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1iued72s1qca17be0vs721jpv6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://ii6f8wjsic.execute-api.us-east-2.amazonaws.com/rider-dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl:  ''//'https://ii6f8wjsic.execute-api.us-east-2.amazonaws.com/rider-dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
